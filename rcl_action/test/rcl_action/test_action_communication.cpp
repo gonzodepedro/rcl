3b8e595d8652370e4245e61b69bbd3810ea432c2
William Woodall <william@osrfoundation.org>
2018-11-20 20:31:50 -0600

    updates after rebase
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rcl_action/test/rcl_action/test_action_communication.cpp
+++ rcl_action/test/rcl_action/test_action_communication.cpp
@@ -34,11 +34,16 @@ protected:
   void SetUp() override
   {
     rcl_allocator_t allocator = rcl_get_default_allocator();
-    rcl_ret_t ret = rcl_init(0, nullptr, allocator);
+    rcl_ret_t ret;
+    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    ret = rcl_init_options_init(&init_options, allocator);
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
+    context = rcl_get_zero_initialized_context();
+    ret = rcl_init(0, nullptr, &init_options, &context);
     ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
     this->node = rcl_get_zero_initialized_node();
     rcl_node_options_t node_options = rcl_node_get_default_options();
-    ret = rcl_node_init(&this->node, "test_action_communication_node", "", &node_options);
+    ret = rcl_node_init(&this->node, "test_action_communication_node", "", &context, &node_options);
     ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
     ret = rcl_clock_init(RCL_STEADY_TIME, &this->clock, &allocator);
     const rosidl_action_type_support_t * ts = ROSIDL_GET_ACTION_TYPE_SUPPORT(
@@ -60,7 +65,7 @@ protected:
     EXPECT_EQ(ret, RCL_RET_OK);
     ret = rcl_node_fini(&this->node);
     EXPECT_EQ(ret, RCL_RET_OK);
-    ret = rcl_shutdown();
+    ret = rcl_shutdown(&context);
     EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
   }
 
@@ -79,6 +84,7 @@ protected:
   }
 
   rcl_action_server_t action_server;
+  rcl_context_t context;
   rcl_node_t node;
   rcl_clock_t clock;
 };  // class TestActionCommunication
